dd0e7566588e9befb078c360a9e829cea3a0bd19
HypertextAssassin <mridul2598@gmail.com>
2021-06-28 17:47:41 +0530

    Updated

---
+++ OdeToFood.data/OdeToFoodDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.data
+{
+    class OdeToFoodDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
